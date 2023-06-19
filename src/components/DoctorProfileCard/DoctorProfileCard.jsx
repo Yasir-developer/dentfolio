c33ab7e3e801a45eaed11a2a252b9f34c46ce5c0
Ali Raza Farishta <alirf50@hotmail.com>
2023-06-19 20:53:50 +0500

    font changes

--- src/components/DoctorProfileCard/DoctorProfileCard.jsx
+++ src/components/DoctorProfileCard/DoctorProfileCard.jsx
@@ -48,43 +48,155 @@ const DoctorProfileCard = () => {
     },
   ];
   return (
+    // <div>
+    //   {doctorArray.map((item, index) => {
+    //     return (
+    //       <div
+    //         className="flex flex-row py-8 border-b border-[#70707030] "
+    //         key={doctorArray.id}
+    //       >
+    //         <div>
+    //           <Image
+    //             src={item.img_url}
+    //             width={173}
+    //             height={173}
+    //             className="rounded-[86.5px]"
+    //           />
+    //         </div>
+
+    //         <div className="mx-7">
+    //           <h2 className="text-custom-blue font-bold">{item.name}</h2>
+    //           <div className="flex flex-row py-2">
+    //             <Image src={location} alt="logo" />
+    //             <h2 className="px-1 text-[14px] font-extralight">
+    //               {item.country}
+    //             </h2>
+    //           </div>
+
+    //           <p className="px-1 text-[18px] font-normal w-[90%] ">
+    //             {item.description}
+    //           </p>
+
+    //           <button className="bg-custom-blue font-semibold  text-[16px] py-3 px-[60px] mt-10  w-139 text-sm text-white rounded-[7px]">
+    //             Contact Me
+    //           </button>
+
+    //           <button className="bg-transparent border border-[#252525] font-semibold  text-[16px] py-3 px-[60px] mt-10  w-139 text-sm text-[#252525] rounded-[7px] ml-8">
+    //             View Profile
+    //           </button>
+    //         </div>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
+
+    // <div>
+    //   {doctorArray.map((item, index) => {
+    //     return (
+    //       <div
+    //         className="flex flex-col sm:flex-row py-8 border-b border-[#70707030] "
+    //         key={item.id}
+    //       >
+    //         <div className="mb-4 sm:mb-0 sm:mr-7 flex-shrink-0 flex flex-row items-center ">
+    //           <Image
+    //             src={item.img_url}
+    //             width={173}
+    //             height={173}
+    //             className="rounded-[86.5px]"
+    //           />
+    //           <div className="sm:ml-7">
+    //             <div className="flex flex-col sm:flex-row items-center">
+    //               <div className="flex items-center">
+    //                 <h2 className="text-custom-blue font-bold text-xl sm:text-2xl">
+    //                   {item.name}
+    //                 </h2>
+    //                 <div className="pl-2">
+    //                   <Image src={location} alt="logo" />
+    //                   <h2 className="px-1 text-xs sm:text-sm font-extralight">
+    //                     {item.country}
+    //                   </h2>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //         <div className="flex flex-col sm:flex-row sm:items-center flex-grow">
+    //           <div className="mt-4 sm:mt-0">
+    //             <p className="px-1 text-base sm:text-lg font-normal">
+    //               {item.description}
+    //             </p>
+
+    //             <div className="flex flex-wrap mt-4">
+    //               <button className="bg-custom-blue font-semibold text-base sm:text-lg py-3 px-4 sm:px-[60px] mt-2 sm:mt-0 mr-2 sm:mr-4 text-white rounded-[7px]">
+    //                 Contact Me
+    //               </button>
+
+    //               <button className="bg-transparent border border-[#252525] font-semibold text-base sm:text-lg py-3 px-4 sm:px-[60px] mt-2 sm:mt-0 text-[#252525] rounded-[7px]">
+    //                 View Profile
+    //               </button>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
+
     <div>
       {doctorArray.map((item, index) => {
         return (
           <div
-            className="flex flex-row py-8 border-b border-[#70707030] "
-            key={doctorArray.id}
+            className="flex flex-col py-8 border-b border-[#70707030] "
+            key={item.id}
           >
-            <div>
+            <div className="mb-4 sm:mb-0 sm:mr-7 flex-shrink-0 flex flex-row items-center">
               <Image
                 src={item.img_url}
-                width={173}
-                height={173}
-                className="rounded-[86.5px]"
+                // width={163}
+                // height={163}
+                width="0"
+                height="0"
+                sizes="100vw"
+                className=" w-[115px] h-[115px] lg:w-[173px] lg:h-[173px] lg:rounded-[86.5px] rounded-[57.5px] "
+                // className="rounded-[86.5px] sm:w-[115px] sm:h-[115px]"
               />
-            </div>
 
-            <div className="mx-7">
-              <h2 className="text-custom-blue font-bold">{item.name}</h2>
-              <div className="flex flex-row py-2">
-                <Image src={location} alt="logo" />
-                <h2 className="px-1 text-[14px] font-extralight">
-                  {item.country}
-                </h2>
+              <div className="sm:ml-7">
+                <div className="flex flex-col  items-start p-[20px]">
+                  <h2 className="text-custom-blue font-semibold text-[18px] lg:text-[21px]">
+                    {item.name}
+                  </h2>
+                  <div className="flex mt-3">
+                    <Image src={location} alt="logo" />
+                    <h2 className="px-1 text-xs sm:text-sm font-extralight">
+                      {item.country}
+                    </h2>
+                  </div>
+                  <p className="px-1 text-base sm:text-lg font-normal hidden lg:block lg:my-5">
+                    {item.description}
+                  </p>
+                </div>
               </div>
+            </div>
+            <p className="px-1 text-base sm:text-lg font-normal lg:hidden">
+              {item.description}
+            </p>
 
-              <p className="px-1 text-[18px] font-normal w-[90%] ">
-                {item.description}
-              </p>
+            {/* <div className="flex flex-col sm:flex-row sm:items-center flex-grow">
+              <div className="mt-4 sm:mt-0"> */}
 
-              <button className="bg-custom-blue font-semibold  text-[16px] py-3 px-[60px] mt-10  w-139 text-sm text-white rounded-[7px]">
+            <div className="flex  mt-4 lg:w-[85%] w-[100%] justify-between lg:justify-center">
+              <button className="bg-custom-blue lg:font-semibold font-medium text-base sm:text-lg w-[50%] lg:w-auto  text-[14px] py-3 px-[30px] sm:px-[60px] mt-2 sm:mt-0 mr-2 sm:mr-4 text-white rounded-[7px]">
                 Contact Me
               </button>
 
-              <button className="bg-transparent border border-[#252525] font-semibold  text-[16px] py-3 px-[60px] mt-10  w-139 text-sm text-[#252525] rounded-[7px] ml-8">
+              <button className="bg-transparent border border-[#252525] lg:font-semibold font-medium text-base w-[50%] lg:w-auto sm:text-lg py-3 px-[30px] sm:px-[60px] mt-2 sm:mt-0 text-[#252525] rounded-[7px]">
                 View Profile
               </button>
             </div>
+            {/* </div>
+            </div> */}
           </div>
         );
       })}
--- src/pages/dentist/dentist-list.jsx
+++ src/pages/dentist/dentist-list.jsx
@@ -5,7 +5,7 @@ import { FaFilter, FaCrosshairs } from "react-icons/fa";
 const dentistlist = () => {
   return (
     <div className="bg-blue sizingStyles mt-8">
-      <h1 className="text-4xl font-bold mb-4 text-custom-black">
+      {/* <h1 className="text-4xl font-bold mb-4 text-custom-black">
         TOP{" "}
         <span className="text-4xl font-poppins text-custom-blue">
           CERTIFIED
@@ -32,7 +32,6 @@ const dentistlist = () => {
           <div className="border border-gray-300 w-full flex items-center rounded-[5px] pr-[15px]">
             <FaCrosshairs size={20} color="#000" className="ml-2" />
 
-            {/* <BiCurrentLocation size={24} color="#000" /> */}
 
             <input
               type="text"
@@ -42,11 +41,46 @@ const dentistlist = () => {
             <FaFilter size={16} color="#000" />
           </div>
         </div>
+      </div> */}
+
+      <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-custom-black">
+        TOP{" "}
+        <span className="text-3xl sm:text-4xl font-poppins text-custom-blue">
+          CERTIFIED
+        </span>{" "}
+        DENTIST IN{" "}
+        <span className="text-3xl sm:text-4xl font-poppins text-custom-blue">
+          MANCHESTER
+        </span>
+      </h1>
+
+      <h3 className="text-lg sm:text-xl font-normal">
+        25 Dentist available in Manchester
+      </h3>
+      <div className="w-full sm:w-[80%]">
+        <p className="mt-8 text-base sm:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          congue, sapien non efficitur sollicitudin, ex risus semper diam, sed
+          ornare libero urna ac leo.
+        </p>
+      </div>
+      <div className="bg-white rounded-[7px] w-full sm:w-[35%]">
+        <div className="flex items-center py-4 sm:py-[30px] w-full">
+          <div className="border border-gray-300 w-full flex items-center rounded-[5px] pr-[15px]">
+            <FaCrosshairs size={20} color="#000" className="ml-2" />
+            <input
+              type="text"
+              placeholder="Location"
+              className="flex-grow py-2 px-4 focus:outline-none w-4/5 text-sm sm:text-base"
+            />
+            <FaFilter size={16} color="#000" />
+          </div>
+        </div>
       </div>
 
       <DoctorProfileCard />
       <div className="flex justify-center my-[80px]">
-        <button className="bg-custom-blue font-semibold text-[16px] py-3 px-[60px] mt-10  w-139 text-sm text-white rounded-[7px] ">
+        <button className="bg-custom-blue lg:font-semibold font-medium text-[16px] py-3 px-[60px] lg:mt-5  w-139 text-sm text-white rounded-[7px] ">
           Load More{" "}
         </button>
       </div>
