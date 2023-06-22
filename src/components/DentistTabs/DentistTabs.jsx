010fabf4a54e79c48b0f43837085ce54281346f7
yasir <yasirupworkdev@gmail.com>
2023-06-22 22:49:38 +0500

    dashboard tab work

--- src/components/DentistTabs/DentistTabs.jsx
+++ src/components/DentistTabs/DentistTabs.jsx
@@ -1,8 +1,11 @@
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 import logoWhite from "../../../public/images/logoWhite.png";
 import { FaIdCard, FaUser, FaFileAlt, FaEdit, FaCog } from "react-icons/fa";
-import Router, { useRouter } from "next/router";
+// import Router, { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
 import Link from "next/link";
 import DashboardHeader from "../DashboardHeader/DashboardHeader";
 import EditCasePage from "@/page-components/EditCasePage";
@@ -10,17 +13,18 @@ import DashboardFooter from "../DashboardFooter/DashboardFooter";
 import ViewProfilePage from "@/page-components/ViewProfilePage";
 import CreateCasePage from "@/page-components/CreateCasePage";
 import BlueButtons from "../Buttons/BlueButtons";
+import SettingsPage from "@/page-components/SettingsPage";
 const DentistTabs = () => {
   const router = useRouter();
 
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState("editP");
 
-  const handleTabClick = (event, path) => {
-    console.log(path, "path >>>>>>>>>>>>>>");
-    event.preventDefault();
+  // const handleTabClick = (event, path) => {
+  //   console.log(path, "path >>>>>>>>>>>>>>");
+  //   event.preventDefault();
 
-    router.push(path);
-  };
+  //   router.push(path);
+  // };
 
   return (
     <div className="flex flex-wrap h-full">
@@ -45,7 +49,7 @@ const DentistTabs = () => {
                 onClick={(e) => {
                   setActiveTab("editProfile");
 
-                  handleTabClick(e, "/dentist/view-profile");
+                  // handleTabClick(e, "/dentist/view-profile");
                 }}
               >
                 <FaIdCard className="w-4 h-4 mx-2" />
@@ -59,9 +63,10 @@ const DentistTabs = () => {
                   "bg-white text-custom-blue rounded-[5px] w-[150px]"
                 }`}
                 onClick={(e) => {
+                  e.preventDefault();
                   setActiveTab("viewProfile");
 
-                  handleTabClick(e, "/dentist/view-profile");
+                  // handleTabClick(e, "/dentist/view-profile");
                 }}
               >
                 <FaUser className="w-4 h-4 mx-2" />
@@ -76,7 +81,7 @@ const DentistTabs = () => {
                   "bg-white text-custom-blue rounded-[5px]  py-3"
                 }`}
                 onClick={(e) => {
-                  handleTabClick(e, "/dentist/create-case");
+                  // handleTabClick(e, "/dentist/create-case");
                   setActiveTab("createCase");
                 }}
               >
@@ -93,7 +98,7 @@ const DentistTabs = () => {
                   "bg-white text-custom-blue rounded-[5px] py-3"
                 }`}
                 onClick={(e) => {
-                  handleTabClick(e, "/dentist/edit-case");
+                  // handleTabClick(e, "/dentist/edit-case");
                   setActiveTab("editCase");
                 }}
               >
@@ -107,7 +112,7 @@ const DentistTabs = () => {
                   activeTab === "settings" &&
                   "bg-white text-custom-blue rounded-[5px] w-[150px] py-3"
                 }`}
-                onClick={(e) => handleTabClick(e, "settings")}
+                // onClick={(e) => handleTabClick(e, "settings")}
               >
                 <FaCog className="w-4 h-4 mx-2" />
                 Settings
@@ -123,100 +128,38 @@ const DentistTabs = () => {
         </div>
         <div className="bg-gray-100 w-[85%] flex flex-col justify-between">
           <div className="contentContainer overflow-y-scroll h-full">
-            {activeTab === "editCase" && <EditCasePage />}
+            {activeTab === "editCase" && (
+              <EditCasePage />
+              // <Link href="/dentist/edit-case" passHref />
+            )}
             {activeTab === "createCase" && <CreateCasePage />}
             {activeTab === "editProfile" && <ViewProfilePage />}
-            {activeTab === "viewProfile" && <ViewProfilePage />}
-            {activeTab === "settings" && <h2>Settings Content</h2>}
+            {
+              activeTab === "viewProfile" &&
+                router.push("/patient/profile-page")
+              // <ViewProfilePage />
+              // <Link href="/dentist/view-profile" passHref />
+            }
+            {activeTab === "settings" && <SettingsPage />}
           </div>
           <div className="footerContainer">
             <DashboardFooter />
           </div>
         </div>
       </div>
-
-      {/* Main Content */}
-
-      {/* <div className="w-64 bg-gradient-radial from-[#0372E2] to-[#0B5FB4]">
-        <div className="p-4">
-          <Image src={logoWhite} className="mx-auto" />
-
-        </div>
-        <nav className="p-2">
-          <ul>
-            <li
-              className={`py-2 px-4 cursor-pointer flex flex-row items-center ml-2 text-[16px] font-semibold ${
-                activeTab !== "editCase" && "text-white"
-              } ${
-                activeTab === "editCase" &&
-                "bg-white text-custom-blue rounded-[5px] w-[150px]"
-              }`}
-              onClick={() => handleTabClick("editCase")}
-            >
-              <FaIdCard className="w-4 h-4 mx-2" />
-              Edit Case
-            </li>
-            <li
-              className={`py-2 px-4 cursor-pointer flex flex-row items-center ml-2 text-[16px] font-semibold ${
-                activeTab !== "createCase" && "text-white"
-              } ${
-                activeTab === "createCase" &&
-                "bg-white text-custom-blue rounded-[5px] w-[150px]"
-              }`}
-              onClick={() => handleTabClick("createCase")}
-            >
-              <FaUser className="w-4 h-4 mx-2" />
-              Create Case
-            </li>
-            <li
-              className={`py-2 px-4 cursor-pointer flex flex-row items-center ml-2 text-[16px] font-semibold ${
-                activeTab !== "editProfile" && "text-white"
-              } ${
-                activeTab === "editProfile" &&
-                "bg-white text-custom-blue rounded-[5px] w-[150px]"
-              }`}
-              onClick={() => handleTabClick("editProfile")}
-            >
-              <FaFileAlt className="w-4 h-4 mx-2" />
-              Edit My Profile
-            </li>
-            <li
-              className={`py-2 px-4 cursor-pointer flex flex-row items-center ml-2 text-[16px] font-semibold ${
-                activeTab !== "viewProfile" && "text-white"
-              } ${
-                activeTab === "viewProfile" &&
-                "bg-white text-custom-blue rounded-[5px] w-[150px]"
-              }`}
-              onClick={() => handleTabClick("viewProfile")}
-            >
-              <FaEdit className="w-4 h-4 mx-2" />
-              View My Profile
-            </li>
-            <li
-              className={`py-2 px-4 cursor-pointer flex flex-row items-center ml-2 text-[16px] font-semibold ${
-                activeTab !== "settings" && "text-white"
-              } ${
-                activeTab === "settings" &&
-                "bg-white text-custom-blue rounded-[5px] w-[150px]"
-              }`}
-              onClick={() => handleTabClick("settings")}
-            >
-              <FaCog className="w-4 h-4 mx-2" />
-              Settings
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <div className="flex-grow bg-gray-100">
-        {activeTab === "editCase" && <h2>Edit Case Content</h2>}
-        {activeTab === "createCase" && <h2>Create Case Content</h2>}
-        {activeTab === "editProfile" && <h2>Edit My Profile Content</h2>}
-        {activeTab === "viewProfile" && <h2>View My Profile Content</h2>}
-        {activeTab === "settings" && <h2>Settings Content</h2>}
-      </div> */}
     </div>
   );
 };
+
+// export async function getStaticProps({ params }) {
+//   const props = await getData(params);
+
+//   // key is needed here
+//   props.key = data.id;
+
+//   return {
+//     props: props,
+//   };
+// }
 
 export default DentistTabs;
--- src/pages/dentist/settings.jsx
+++ src/pages/dentist/settings.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+import logoWhite from "../../../public/images/logoWhite.png";
+import { FaIdCard, FaUser, FaFileAlt, FaEdit, FaCog } from "react-icons/fa";
+import Router from "next/router";
+import Link from "next/link";
+import DentistTabs from "@/components/DentistTabs/DentistTabs";
+import EditCasePage from "@/page-components/EditCasePage";
+import ViewProfilePage from "@/page-components/ViewProfilePage";
+import SettingsPage from "@/page-components/SettingsPage";
 
-const settings = () => {
-  return <div className="dentistBodyStyles">settings</div>;
+const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState("editCase");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="dentistBodyStyles">
+      {/* Menu Panel */}
+
+      <DentistTabs>
+        <SettingsPage />
+      </DentistTabs>
+
+      {/* <hr className="w-full border-[#70707030] my-20" /> */}
+    </div>
+  );
 };
 
-export default settings;
+export default Dashboard;
