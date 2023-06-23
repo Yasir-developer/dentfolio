a9bace784917d14382a102d3b0c241837506b399
mkarrar1998 <86237345+mkarrar1998@users.noreply.github.com>
2023-06-23 21:19:20 +0500

    Edit case card responsive done

--- src/components/EditCaseCard/EditCaseCard.jsx
+++ src/components/EditCaseCard/EditCaseCard.jsx
@@ -9,7 +9,7 @@ const EditCaseCard = ({ img_url, name, description, id }) => {
       <div className="mb-10">
         {/* <div> */}
         <div
-          className="border rounded-[7px] border-[#F6EBEB] border-b flex flex-col-reverse lg:flex-row px-3 mt-7"
+          className="border rounded-[7px] border-[#F6EBEB] relative flex lg:flex-row flex-col-reverse px-3 mt-7"
           key={id}
         >
           <Image
@@ -21,22 +21,25 @@ const EditCaseCard = ({ img_url, name, description, id }) => {
             className="w-full h-full max-w-[300px] lg:w-auto lg:h-[170px] my-3"
           />
 
-          <div className="flex flex-col lg:px-5 pt-5 pb-5 items-start">
-            <h2 className="text-center lg:text-left text-[21px] text-custom-blue font-semibold my-2">
+          <div className="flex flex-wrap lg:px-5 pt-5 pb-5 items-center lg:items-start">
+            <h2 className="lg;text-center lg:text-left text-[21px] text-custom-blue font-semibold my-2 lg:w-full w-[90%]">
               {name}
             </h2>
+            <FaEllipsisV
+            className="static w-[5%] lg:absolute lg:right-0 lg:top-0 lg:w-[18px] lg:h-[18px] font-black lg:mx-[3px] lg:mb-[3px] lg:mt-[5px]"
+          />
 
-            <p className="text-center lg:text-left text-[12px] lg:text-[16px] font-light">
+            <p className="text-left text-[12px] lg:text-[16px] font-light w-full">
               {description}
             </p>
-            <div className="justify-start">
+            <div className="justify-start w-full">
               <BlueButtons
                 buttonText={"Edit"}
-                className={"mx-auto my-2 px-10"}
+                className={"mx-auto my-2 !px-10"}
               />
             </div>
           </div>
-          <FaEllipsisV
+          {/* <FaEllipsisV
             style={{
               margin: "3px",
               marginTop: "5px",
@@ -44,7 +47,7 @@ const EditCaseCard = ({ img_url, name, description, id }) => {
               width: "18px",
               height: "18px",
             }}
-          />
+          /> */}
         </div>
       </div>
       {/* </div> */}
