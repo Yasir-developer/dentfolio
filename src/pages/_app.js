0c951853f3326d56229b88bd10f2190412e95340
yasir <yasirupworkdev@gmail.com>
2023-07-07 00:00:06 +0500

    progress bar added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,29 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-datepicker/dist/react-datepicker.css";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import "../styles/nprogress.css";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 500,
+    showSpinner: false,
+  });
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => NProgress.start());
+    router.events.on("routeChangeComplete", () => NProgress.done());
+    router.events.on("routeChangeError", () => NProgress.done());
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
